76d48271e1c44b47c6db3e43225caf871b1cf732
Alexey <alexeyvakarchuk@yandex.ru>
2017-09-27 22:27:05 +0300

    Remove propTypes

--- div-icon.js
+++ div-icon.js
@@ -4,91 +4,94 @@ import {DivIcon, marker} from 'leaflet';
 import {MapLayer} from 'react-leaflet';
 
 function createContextProvider(context) {
-  class ContextProvider extends Component {
-    getChildContext() {
-      return context;
-    }
+    class ContextProvider extends Component {
+        getChildContext() {
+            return context;
+        }
 
-    render() {
-      return this.props.children;
+        render() {
+            return this.props.children;
+        }
     }
-  }
-
-  ContextProvider.childContextTypes = {};
-  Object.keys(context).forEach(key => {
-    ContextProvider.childContextTypes[key] = PropTypes.any;
-  });
-  return ContextProvider;
+    //
+    // ContextProvider.childContextTypes = {};
+    // Object.keys(context).forEach(key => {
+    //     ContextProvider.childContextTypes[key] = PropTypes.any;
+    // });
+    return ContextProvider;
 }
 
 export default class Divicon extends MapLayer {
-  getChildContext() {
-    return {
-      popupContainer: this.leafletElement,
+    // static childContextTypes = {
+    //     popupContainer: PropTypes.object,
+    // };
+    //
+    // getChildContext() {
+    //     return {
+    //         popupContainer: this.leafletElement,
+    //     }
+    // }
+
+    // See https://github.com/PaulLeCam/react-leaflet/issues/275
+    createLeafletElement(newProps) {
+        const {map: _map, layerContainer: _lc, position, ...props} = newProps;
+        this.icon = new DivIcon(props);
+        return marker(position, {icon: this.icon, ...props});
     }
-  }
 
-  // See https://github.com/PaulLeCam/react-leaflet/issues/275
-  createLeafletElement(newProps) {
-    const {map: _map, layerContainer: _lc, position, ...props} = newProps;
-    this.icon = new DivIcon(props);
-    return marker(position, {icon: this.icon, ...props});
-  }
-
-  updateLeafletElement(fromProps, toProps) {
-    if (toProps.position !== fromProps.position) {
-      this.leafletElement.setLatLng(toProps.position);
+    updateLeafletElement(fromProps, toProps) {
+        if (toProps.position !== fromProps.position) {
+            this.leafletElement.setLatLng(toProps.position);
+        }
+        if (toProps.zIndexOffset !== fromProps.zIndexOffset) {
+            this.leafletElement.setZIndexOffset(toProps.zIndexOffset);
+        }
+        if (toProps.opacity !== fromProps.opacity) {
+            this.leafletElement.setOpacity(toProps.opacity);
+        }
+        if (toProps.draggable !== fromProps.draggable) {
+            if (toProps.draggable) {
+                this.leafletElement.dragging.enable();
+            }
+            else {
+                this.leafletElement.dragging.disable();
+            }
+        }
     }
-    if (toProps.zIndexOffset !== fromProps.zIndexOffset) {
-      this.leafletElement.setZIndexOffset(toProps.zIndexOffset);
+
+    componentWillMount() {
+        super.componentWillMount();
+        this.leafletElement = this.createLeafletElement(this.props);
     }
-    if (toProps.opacity !== fromProps.opacity) {
-      this.leafletElement.setOpacity(toProps.opacity);
+
+    componentDidMount() {
+        super.componentDidMount();
+        this.renderComponent();
     }
-    if (toProps.draggable !== fromProps.draggable) {
-      if (toProps.draggable) {
-        this.leafletElement.dragging.enable();
-      }
-      else {
-        this.leafletElement.dragging.disable();
-      }
+
+    componentDidUpdate(fromProps) {
+        this.renderComponent();
+        this.updateLeafletElement(fromProps, this.props);
     }
-  }
 
-  componentWillMount() {
-    super.componentWillMount();
-    this.leafletElement = this.createLeafletElement(this.props);
-  }
-
-  componentDidMount() {
-    super.componentDidMount();
-    this.renderComponent();
-  }
-
-  componentDidUpdate(fromProps) {
-    this.renderComponent();
-    this.updateLeafletElement(fromProps, this.props);
-  }
-
-  renderComponent = () => {
-    const ContextProvider = createContextProvider({...this.context, ...this.getChildContext()});
-    const container = this.leafletElement._icon;
-    const component = (
-      <ContextProvider>
-        {this.props.children}
-      </ContextProvider>
-    );
-    if (container) {
-      render(
-        component,
-        container
-      );
+    renderComponent = () => {
+        const ContextProvider = createContextProvider({...this.context, ...this.getChildContext()});
+        const container = this.leafletElement._icon;
+        const component = (
+            <ContextProvider>
+                {this.props.children}
+            </ContextProvider>
+        );
+        if (container) {
+            render(
+                component,
+                container
+            );
+        }
     }
-  }
 
-  render() {
-    return null;
-  }
+    render() {
+        return null;
+    }
 
 }
-
